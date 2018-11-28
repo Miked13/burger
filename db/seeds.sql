6cd909e7c5c49265a95461e1f25818345f8b9bbb
Scorpio <diallomikey13@yahoo.fr>
2018-11-27 23:41:16 -0500

    MVC update

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Texas Burger');
+INSERT INTO burgers (burger_name) VALUES ('Toxic Burger');
+INSERT INTO burgers (burger_name) VALUES ('Blazing Burger');
+INSERT INTO burgers (burger_name) VALUES ('Megazaur Burger');
+INSERT INTO burgers (burger_name) VALUES ('Classy Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Christmas Burger', true);
